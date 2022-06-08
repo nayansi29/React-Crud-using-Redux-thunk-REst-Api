3488c0da2683853555594535d07358c2dcfcd797
nancy <nancypurwar5@gmail.com>
2022-06-08 18:01:48 +0530

    deleted some files

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import StreamList from './components/streams/StreamList';
 import { Router, Route } from 'react-router-dom';
 import StreamEdit from './components/streams/StreamEdit';
-import StreamDelete from './components/streams/StreamDelete';
-import StreamShow from './components/streams/StreamShow';
 import Header from './components/Header';
 import StreamCreate from './components/streams/StreamCreate';
 import history from './history';
@@ -14,8 +12,7 @@ function App() {
         <Route path="/" exact component={StreamList} />
         <Route path="/stream/new" exact component={StreamCreate} />
         <Route path="/stream/edit/:id" exact component={StreamEdit} />
-        <Route path="/stream/delete" exact component={StreamDelete} />
-        <Route path="/stream/show" exact component={StreamShow} />
+
 
       </Router>
     </div>
